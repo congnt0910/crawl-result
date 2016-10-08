f2c3618a639131d261260509601d641789e2d19c
Thành Công Nguyễn <nthanhcong0910@gmail.com>
2016-10-08 18:55:54 +0700

    note

---
+++ analytical/think.md
@@ -0,0 +1,8 @@
+
+#### solution create schedule:
+ 
+1) tao schedule cho tat ca cac loai khi chay
+2) tao schedule chay moi ngay vao 1 gio (gio nay dam bao ko co loai xs nao mo thuong) de xac dinh trong ngay co loai
+ xs nao mo thuong -> tao schedule cho cac loai xs mo thuong trong ngay
+ 
+ (1) & (2) -> tao tiep schedule quet sau moi 5' khi schedule moi loai chay
--- src/core/index.js
+++ src/core/index.js
@@ -62,12 +62,7 @@ const run = () => {
 }
 
 // TODO: nhận đầu vào là 1 này -> Quét lấy dữ liệu tất cả loại xổ số mở thưởng ngày đó lưu vào db
-
-// solution create schedule:
-//      1) tao schedule cho tat ca cac loai khi chay
-//      2) tao schedule chay moi ngay vao 1 gio (gio nay dam bao ko co loai xs nao mo thuong) de xac dinh trong ngay co loai
-//        xs nao mo thuong -> tao schedule cho cac loai xs mo thuong trong ngay
-// (1) & (2) -> tao tiep schedule quet sau moi 5' khi schedule moi loai chay
+// TODO: tao api de control tu ui
 
 export {
   processMainSchedule,
