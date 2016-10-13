b0758485639ccd38ccb23e7c6ba706afe5e69189
Thành Công Nguyễn <nthanhcong0910@gmail.com>
2016-10-13 23:23:27 +0700

    update docs

--- src/database/knex.js
+++ src/database/knex.js
@@ -10,13 +10,50 @@ import connect, {
 
 const conn = connect()
 
+/**
+ * Module connect to database
+ * @module database/knex
+ */
 export {
+  /**
+   * Connection
+   * @type {Knex}
+   */
   conn,
+  /**
+   * Table name
+   * @type {Object}
+   */
   tableName,
+  /**
+   * Wait to connection
+   * @function
+   * @returns {Promise}
+   */
   waitDbReady,
+  /**
+   * Truncate all table
+   * @function
+   * @returns {Promise}
+   */
   truncateAllTable,
+  /**
+   * Drop database
+   * @function
+   * @returns {Promise}
+   */
   dropDatabase,
+  /**
+   * Create schema
+   * @function
+   * @returns {Promise}
+   */
   createSchema,
+  /**
+   * Create database
+   * @function
+   * @returns {Promise}
+   */
   createDatabase
 }
 
